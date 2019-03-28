90ceb626ba86c40f9ff5a27db60e43ae0adb71c1
Brett W <brett9000@icloud.com>
2019-03-28 09:04:09 -0700

    updated path

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-echo "Installing bashdot."
+echo "Installing bashdot as '~/bashdot'."
 url=https://raw.githubusercontent.com/bashdot/bashdot/4.0.1/bashdot
-curl -s $url > bashdot
-chmod 755 bashdot
+curl -s $url > ~/bashdot
+chmod 755 ~/bashdot
 
-echo "Cloning public dotfiles."
-git clone https://github.com/weavenet/dotfiles
+echo "Cloning public dotfiles into '~/dotfiles'."
+git clone https://github.com/weavenet/dotfiles ~/dotfiles
 
 echo "Installing default profile."
-bash bashdot install dotfiles
+bash ~/bashdot install dotfiles
 
 echo "Dotfiles installed, run '. ~/.bashrc' to source dotfiles."
