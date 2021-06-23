419870b0ab5dcddf4a3e93ff8d0bc1eb5dd30255
Brett W <brett9000@icloud.com>
2021-06-23 06:08:05 -0700

    adding basic output

--- scripts/set_aws_env_from_profile.sh
+++ scripts/set_aws_env_from_profile.sh
@@ -1,5 +1,8 @@
 if [ -n "${AWS_PROFILE}" ]; then
+    echo "Reading from profile '${AWS_PROFILE}'."
     export AWS_ACCESS_KEY_ID=`aws configure get $AWS_PROFILE.aws_access_key_id`
     export AWS_SECRET_ACCESS_KEY=`aws configure get $AWS_PROFILE.aws_secret_access_key`
     export AWS_SESSION_TOKEN=`aws configure get $AWS_PROFILE.aws_session_token`
+else
+    echo "AWS_PROFILE not set"
 fi
